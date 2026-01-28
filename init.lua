812adeb959d69799093f4ba2df432f3a0ed54967
Velan <228343634+velan-code@users.noreply.github.com>
2026-01-29 00:26:56 +0530

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,5 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("plugins.color")
+require("plugins.tmux")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+vim.g.python3_host_prog = "/usr/bin/python3"
+
+--[[require("nvim-treesitter").setup({
+  ensure_installed = { "python", "bash" },
+  highlight = {
+    enable = true,
+  },
+})
+]]
